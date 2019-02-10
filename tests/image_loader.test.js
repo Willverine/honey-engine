2cb38af791aa73f57260edc8edd900f0e1ab12d5
William Owers <owers50@gmail.com>
2019-02-10 12:46:42 +0800

    Added image loader spec

--- tests/image_loader.test.js
+++ tests/image_loader.test.js
@@ -1,9 +1,14 @@
 const ImageLoader = require('../lib/image_loader')
+const SpriteSheet = require('../lib/sprite_sheet')
 // console = require('../lib/helpers.js').console
 // global.document = require('../lib/helpers.js').document
-canvasMock = require('../lib/helpers.js').canvasMock
+// canvasMock = require('../lib/helpers.js').canvasMock
+canvasMock = createCanvas(200,200)
 
-test('image loader returns a ', function () {
+test('image loader creates a new spritesheet', function () {
   // expect(clientx.screenLength).toBe(0)
-  expect(ImageLoader('./test.jpg', canvasMock, canvasMock)).toBe(undefined)
+  let testPath = './test_fixtures/test.jpg'
+  let newImage = ImageLoader(testPath, canvasMock, canvasMock)
+  expect(newImage).toBe(undefined)
+  expect(SpriteSheet.prototype.hasImage(testPath)).toBe(true)
 })
